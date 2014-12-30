b2e0b5694ce797ec0c1171b5ded94e04f8f54fed
Micah Thomas <micahthomas4@gmail.com>
2014-12-30 11:44:07 -0600

    Fixed autostart and server startup bugs
    removed mavinit.scr, and uses the init.d script to provide the initial commands for mavproxy

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          ravnd
-# Required-Start:    
+# Required-Start:    networking
 # Required-Stop:     
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
@@ -9,23 +9,43 @@
 # Description:       Starts the RAVN Server on any USB device, and if sucessfull
 #                    exits the script
 ### END INIT INFO
-for x in `ls /dev/ttyUSB*`
-do
-    > PIDFILE;
-    mavproxy.py --master=$x;
-    sleep 10;
-    if [ -s PIDFILE ]
-    then
-        exit 0;
-    fi
-done
-for x in `ls /dev/ttyA*`
-do
-    > PIDFILE;
-    mavproxy.py --master=$x;
-    sleep 10;
-    if [ -s PIDFILE ]
-    then
-        exit 0;
-    fi
-done
+start () {
+    shopt -s nullglob
+    a=(/dev/ttyA*)
+    b=(/dev/ttyU*)
+    shopt -u nullglob # Turn off nullglob to make sure it doesn't interfere with anything later
+    array=("${a[@]}" "${b[@]}")
+    for x in $array
+    do
+        > PIDFILE;
+        mavproxy.py --master=$x --cmd="module load droneapi.module.api; api start SERVER"&;
+        sleep 15;
+        if [ -s PIDFILE ]
+        then
+            return 0;
+        else
+            killall -9 mavproxy.py
+        fi
+    done
+}
+stop () {
+    kill -9 $(cat PIDFILE)
+}case "$1" in
+  start)
+        start
+        ;;
+  stop)
+        stop
+        ;;
+  status)
+        status FOO
+        ;;
+  restart|reload|condrestart)
+        stop
+        start
+        ;;
+  *)
+        echo $"Usage: $0 {start|stop|restart|reload|status}"
+        exit 1
+esac
+exit 0
